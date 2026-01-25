8b860c537945ff17b766d4a9d188e32745912df3
Rodrigo <rdesouza2008@gmaill.com>
2026-01-25 15:19:39 -0300

    chore: create api

---
+++ server/FinancialControl.Api/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet(
+    "/test",
+    () =>
+    {
+        return new { Message = "Success" };
+    }
+);
+
+app.Run();
